3a817f10028e66d20e4f179172a9e659649718ac
Petr Pucil <petr.pucil@seznam.cz>
2020-04-30 11:05:01 +0200

    Improve test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+import sys
 from hello_world import HelloWorld
 
 hw = HelloWorld.from_file("file.bin")
-print("Hello, {}".format(hw.one))
+print("one = {}".format(hw.one))
+
+if hw.one == 137:
+    print("[PASS]: one == 137")
+    sys.exit(0)
+else:
+    print("[FAIL]: one != 137")
+    sys.exit(1)
