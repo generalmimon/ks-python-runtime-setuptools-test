55923ce01496f5ec5a439633e3afa24c35714593
Petr Pucil <petr.pucil@seznam.cz>
2020-10-13 23:36:32 +0200

    Benchmark https://github.com/kaitai-io/kaitai_struct_python_runtime/pull/42

--- test.py
+++ test.py
@@ -1,12 +1,34 @@
-import sys
-from hello_world import HelloWorld
+from kaitaistruct import KaitaiStream
 
-hw = HelloWorld.from_file("file.bin")
-print("one = {}".format(hw.one))
+import time
+import random
+import statistics
 
-if hw.one == 137:
-    print("[PASS]: one == 137")
-    sys.exit(0)
-else:
-    print("[FAIL]: one != 137")
-    sys.exit(1)
+len_blob = (2 ** 20) // 4 # 0.25 MiB
+num_sections = 2
+len_section = len_blob // num_sections
+
+test_blob = bytes([i // len_section for i in range(len_blob)])
+
+functions = KaitaiStream.bytes_strip_right, KaitaiStream.bytes_terminate
+times = {f.__name__: [] for f in functions}
+
+# https://www.peterbe.com/plog/how-to-do-performance-micro-benchmarks-in-python
+for i in range(2048):
+    f_idx = random.randint(0, 1)
+
+    t0 = time.time()
+    if f_idx == 0:
+        name = 'bytes_strip_right'
+        KaitaiStream.bytes_strip_right(test_blob, 1)
+    else:
+        name = 'bytes_terminate'
+        KaitaiStream.bytes_terminate(test_blob, 1, False)
+    t1 = time.time()
+    times[name].append((t1 - t0) * 1000)
+
+for name, numbers in times.items():
+    print('FUNCTION:', name, 'Used', len(numbers), 'times')
+    print('\tMEDIAN', statistics.median(numbers))
+    print('\tMEAN  ', statistics.mean(numbers))
+    print('\tSTDEV ', statistics.stdev(numbers))
