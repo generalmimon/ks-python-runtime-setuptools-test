523fce7940c0efe8ad417c974ca0f90433622e16
Petr Pucil <petr.pucil@seznam.cz>
2020-10-14 09:56:41 +0200

    Change workload generation, reduce size to 128 kiB

--- test.py
+++ test.py
@@ -4,13 +4,15 @@ import time
 import random
 import statistics
 
-len_blob = 512 * (2 ** 10) # 512 kiB
-num_sections = 2
+len_blob = 128 * (2 ** 10) # 128 kiB
+# num_sections = 2
 num_iterations = 1024
 
-len_section = len_blob // num_sections
+# len_section = len_blob // num_sections
 
-test_blob = bytes([i // len_section for i in range(len_blob)])
+# test_blob = bytes([i // len_section for i in range(len_blob)])
+test_blob_strip_right = bytes([42] + [1] * (len_blob - 1))
+test_blob_term = bytes([42] * (len_blob - 1) + [1])
 
 times = {'bytes_strip_right': [], 'bytes_terminate': []}
 
@@ -21,10 +23,10 @@ for i in range(num_iterations):
     t0 = time.time()
     if f_idx == 0:
         name = 'bytes_strip_right'
-        KaitaiStream.bytes_strip_right(test_blob, 1)
+        KaitaiStream.bytes_strip_right(test_blob_strip_right, 1)
     else:
         name = 'bytes_terminate'
-        KaitaiStream.bytes_terminate(test_blob, 1, False)
+        KaitaiStream.bytes_terminate(test_blob_term, 1, False)
     t1 = time.time()
     times[name].append((t1 - t0) * 1000)
 
